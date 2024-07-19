f056e7d5fd128d76b9ed466be33de721bb5be59f
Maina Maureen <146948714+Maureen-Maina@users.noreply.github.com>
2024-07-20 00:05:51 +0300

    Create Add.php
    Pass by value

---
+++ Add.php
@@ -0,0 +1,40 @@
+<html> 
+
+<body> 
+
+<?php 
+
+ //Adding two numbers 
+
+ function add($x, $y) 
+
+{ 
+
+ $sum = $x + $y; 
+
+ echo "Sum of two numbers is = $sum <br><br>"; 
+
+ } 
+
+ add(50, 34); 
+
+ 
+
+ //Subtracting two numbers 
+
+ function sub($x, $y) 
+
+{ 
+
+ $diff = $x - $y; 
+
+ echo "Difference between two numbers is = $diff"; 
+
+ } 
+
+ sub(97, 45);
+?> 
+
+</body> 
+
+</html>
