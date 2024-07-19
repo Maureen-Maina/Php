5050512e6e1e73a5bcf97e81b2b640b3c06552e0
Maina Maureen <146948714+Maureen-Maina@users.noreply.github.com>
2024-07-19 23:58:36 +0300

    Create Name&age.php
    For each loop

---
+++ Name&age.php
@@ -0,0 +1,19 @@
+<html> 
+
+<body background="lime">
+
+<?php
+
+$person = array("Peter"=>"62", "Joyce"=>"58", "Moses"=>"27");
+
+foreach($person as $name => $age) {
+
+ echo "$name = $age<br>";
+
+}
+
+?>
+
+</body> 
+
+</html>
