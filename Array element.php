26b8736d2b9ff4ee9ecc2b7158cf883e70712e0f
Maina Maureen <146948714+Maureen-Maina@users.noreply.github.com>
2024-07-19 23:57:37 +0300

    Create Array element.php
    For each loop

---
+++ Array element.php
@@ -0,0 +1,21 @@
+<html> 
+
+<body>
+
+<?php
+
+$days = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday'];
+
+foreach ($days as $dy) 
+
+{
+
+echo $dy . '<br>';
+
+} 
+
+?> 
+
+</body> 
+
+</html>
