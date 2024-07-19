a473388405c427099edfedf9caa0e377b86825d3
Maina Maureen <146948714+Maureen-Maina@users.noreply.github.com>
2024-07-20 00:12:52 +0300

    Create Product.php
    PHP return Keyword

---
+++ Product.php
@@ -0,0 +1,29 @@
+<html> 
+
+<body> 
+
+<?php
+
+// Defining function
+
+function product($num1, $num2)
+
+{
+
+ $total = $num1 * $num2;
+
+ return $total;
+
+}
+
+// Printing returned value
+
+echo "Product=";
+
+echo product(6, 39); // Outputs: 234
+
+?>
+
+</body> 
+
+</html>
